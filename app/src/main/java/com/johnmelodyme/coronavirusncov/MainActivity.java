6df59158e6fb57974e68c89ff427dadd56599d11
John Melody, Sin Dee <Johnmelodyme@yandex.com>
2020-01-28 11:34:57 +0800

    ActionBar<Items> Added.

--- app/src/main/java/com/johnmelodyme/coronavirusncov/MainActivity.java
+++ app/src/main/java/com/johnmelodyme/coronavirusncov/MainActivity.java
@@ -6,6 +6,9 @@ package com.johnmelodyme.coronavirusncov;
  */
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 /**
  * @Author: John Melody Melissa
  * @Project: Corona Virus nCov
@@ -18,4 +21,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.info) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
+
