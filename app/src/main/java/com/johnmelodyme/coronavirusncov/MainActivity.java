e68358af4e61a821af8c0d07410eaef201073797
John Melody, Sin Dee <Johnmelodyme@yandex.com>
2020-01-28 11:08:50 +0800

    Committed

---
+++ app/src/main/java/com/johnmelodyme/coronavirusncov/MainActivity.java
@@ -0,0 +1,21 @@
+package com.johnmelodyme.coronavirusncov;
+/*
+ * This application is created to keep
+ * people up to date on CORONA VIRUS
+ * during this epidemic event.
+ */
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * @Author: John Melody Melissa
+ * @Project: Corona Virus nCov
+ * @Inpired : SIN DEE 😍 😘 🥰
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
